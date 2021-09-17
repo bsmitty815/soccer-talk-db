782c6219f09f3b988d3e5c605234c725d8ae0917
Brian Smith <brian.bubbam@gmail.com>
2021-09-17 10:10:09 -0500

    create profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,22 @@
 class ProfilesController < ApplicationController
+
+    #PATCH
+    def update
+        user = User.find_by(id: session[:user_id])
+        profile = user.profile
+        if profile
+            profile.update(profile_params)
+            render json: profile, status: 200
+        elsif !user
+            render json: { errors: user.errors.full_messages }, status: :unauthorized
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def profile_params
+        params.permit(:bio, :team)
+    end
 end
