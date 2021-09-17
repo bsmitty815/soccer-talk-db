d0df93a31dff693ea9a1c0c815dbe7f687923dcf
Brian Smith <brian.bubbam@gmail.com>
2021-09-17 11:19:49 -0500

    added validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user
   
+
+  validates :bio, length: { maximum: 500 }
+
+  #add another validation showing only certains names for team
+  validates :team, acceptance: { accept: ['Arsenal', 'Aston Villa', 'Brentford', 'Brighton and Hove Albion', 'Burnley',
+  'Chelsea', 'Crystal Palace', 'Everton', 'Leeds United', 'Leicester City', 'Liverpool', 'Manchester City', 'Manchester United',
+  'Newcastle United', 'Norwich City', 'Southampton', 'Tottenham Hotspur', 'Watford', 'West Ham United', 'Wolverhampton Wanderers'] }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
-    has_secure_password
+    
 
     #do i want to destroy the comments and discussions?
     has_many :discussions, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_one :profile, dependent: :destroy
+
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+
+    #create the profile when a user is created
+    after_create do
+        create_profile(:user_id => self.id, :bio => "", team => "")
+    end
+
+
 end
