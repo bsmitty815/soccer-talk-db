609f4b2af5e0f7714171d2d96e50ba394ee7dc0c
Brian Smith <brian.bubbam@gmail.com>
2021-09-17 09:33:03 -0500

    added sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+
+    #GET login
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+
+    end
+
+
+    #DElETE logout
+    def destroy
+        session.destroy
+        head :no_content
+    end
+    
+end
