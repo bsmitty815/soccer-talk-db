7899a0ebdd2f9207d43c24a3900cac0e153edf19
Brian Smith <brian.bubbam@gmail.com>
2021-09-17 11:22:49 -0500

    added discussion and comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :discussion
+
+  validates :body, length: { maximum: 500 }
 end
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,4 +1,7 @@
 class Discussion < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates :titile, length: { maximum: 200 }
+  validates :body, length: { maximum: 1000 }
 end
