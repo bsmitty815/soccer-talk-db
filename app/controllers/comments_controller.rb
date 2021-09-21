1f5ebe1766b68cb064ab969634253d2433e627cd
Brian Smith <brian.bubbam@gmail.com>
2021-09-21 08:40:19 -0500

    updated discussions create and destroy method, updated comments create and destroy method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+
+    def create
+        comment = Comments.create(comment_params)
+        user = Users.find_by(id: session[:user_id])
+        comment.user_id = user.id
+        if comment
+            render json: comment, status: :created
+        else
+            render json: { errors: comment.error.full_messages }, status: 422
+        end
+    end
+
+    def destroy
+        comment = Comments.find_by(id: params[:id])
+        comment.destroy
+    end
+
+    private
+
+    def comment_params
+        params.permit(:discussion_id, :body)
+    end
 end
--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,2 +1,25 @@
 class DiscussionsController < ApplicationController
+
+    def create
+        discussion = Discussions.create(discussion_params)
+        user = User.find_by(id: session[:user_id])
+        discussion.user_id = user.id
+        if disuccion
+            render json: discussion, status: :created
+        else
+            render json: { errors: discussion.error.full_messages}, status: 422
+        end
+    end
+
+    def destroy
+        discussion = Discussions.find_by(id: params[:id])
+        disuccion.destroy
+    end
+
+
+    private
+
+    def discussion_params
+        params.permit(:title, :body)
+    end
 end
